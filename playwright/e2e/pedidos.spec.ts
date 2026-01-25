d47a805d3dca3c88ab0d05a809d5f66c73599f3f
Pedro Borges <pedroborgespj@gmail.com>
2026-01-25 15:18:48 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test'
 
 test('should consult an approved order', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-0D0081')
   await page.getByTestId('search-order-button').click()
   
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-0D0081')
-
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 

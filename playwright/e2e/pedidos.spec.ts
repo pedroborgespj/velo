2a1e4cdf70120d5a03806775186aa3c280a1c083
Pedro Borges <pedroborgespj@gmail.com>
2026-02-02 21:23:04 -0300

    adding a new test: should show a message when the order is not found

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test'
 
 test('should consult an approved order', async ({ page }) => {
+
+  // Test Data
+  const order = 'VLO-0D0081'
+
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -8,7 +12,7 @@ test('should consult an approved order', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
   
   // Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-0D0081')
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
   // Assert
@@ -19,8 +23,28 @@ test('should consult an approved order', async ({ page }) => {
   const containerPedido = page.getByRole('paragraph')
     .filter({ hasText: /^Pedido$/ })
     .locator('..') // parent element
-  await expect(containerPedido).toContainText('VLO-0D0081', { timeout: 10_000 })
+  await expect(containerPedido).toContainText(order, { timeout: 10_000 })
 
   await expect(page.getByText('APROVADO')).toBeVisible()
 
 });
+
+test('should show a message when the order is not found', async ({ page }) => { 
+
+  const order = 'VLO-ABC123'
+
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
+  
+})

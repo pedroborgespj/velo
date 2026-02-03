d3feaf53ff9c68b6ce69ab03abd8d13d894cdc18
Pedro Borges <pedroborgespj@gmail.com>
2026-02-03 20:19:33 -0300

    assert with snapshots

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -27,14 +27,43 @@ test.describe('Consulta de Pedido', () => {
     // const orderCode = page.locator('//p[text()="Pedido"]/..//p[text()="VLO-0D0081"]')
     // await expect(orderCode).toBeVisible({ timeout: 10_000 })
 
-    const containerPedido = page.getByRole('paragraph')
-      .filter({ hasText: /^Pedido$/ })
-      .locator('..') // parent element
-    await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+    // const containerPedido = page.getByRole('paragraph')
+    //   .filter({ hasText: /^Pedido$/ })
+    //   .locator('..') // parent element
+    // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
 
-    await expect(page.getByText('APROVADO')).toBeVisible()
+    // await expect(page.getByText('APROVADO')).toBeVisible()
 
-  });
+    await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order}
+      - img
+      - text: APROVADO
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Midnight Black
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: sport Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: Pedro Junior
+      - paragraph: Email
+      - paragraph: pedro@velo.dev
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
+
+  })
 
   test('should show a message when the order is not found', async ({ page }) => {
 

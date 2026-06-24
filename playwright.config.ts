6c71711f72cc5b6acc182f5372398e26d377031c
Pedro Borges <pedroborgespj@gmail.com>
2026-06-24 10:41:09 -0300

    add build and deploy via github actions with token secret

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://velo-pedroborgespj.vercel.app',
+    baseURL: 'https://velo-1z19kesho-pedroborgespjs-projects.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
